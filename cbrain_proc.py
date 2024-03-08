b48357c09f6ecff6a6ae2be9153ae8adc61cd283
Erik Lee <leex6144@umn.edu>
2024-03-08 16:04:57 -0600

    [dev] html

--- cbrain_proc.py
+++ cbrain_proc.py
@@ -12,6 +12,9 @@ import pandas as pd
 import botocore
 import datetime 
 import re
+import base64
+from io import BytesIO
+import matplotlib.pyplot as plt
 
 
 
@@ -1738,34 +1741,84 @@ def add_prettier_background_to_html(html_content):
 
     return html_with_css
 
+def append_pie_charts_to_html(html_str, data_dict):
+    """
+    Appends high-resolution pie charts to the end of an HTML string based on a provided dictionary.
+    Each chart has a title, and the legend shows the count for each category.
+
+    Parameters:
+    - html_str: A string containing HTML.
+    - data_dict: A dictionary with titles of plots and data for the pie charts.
+
+    Returns:
+    - A modified HTML string with pie charts appended.
+    """
+
+    # Start the section for pie charts in the HTML
+    new_html = html_str + "\n<div id='pie-charts'>\n"
+
+    for title, data in data_dict.items():
+        # Prepare data for the pie chart
+        labels = list(data.keys())
+        sizes = list(data.values())
+        # Create labels with counts for the legend
+        legend_labels = [f"{label} ({size})" for label, size in zip(labels, sizes)]
+
+        # Create a pie chart
+        fig, ax = plt.subplots()
+        wedges, texts = ax.pie(sizes, startangle=90, counterclock=False)
+        ax.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+        plt.title(title)
+        ax.legend(wedges, legend_labels, title="Categories", loc="center left", bbox_to_anchor=(1, 0, 0.5, 1))
+
+        # Convert the plot to a PNG image, then encode it in base64 at high resolution
+        img_data = BytesIO()
+        plt.savefig(img_data, format='png', dpi=150, bbox_inches="tight")
+        plt.close()  # Close the plot to free memory
+        img_data.seek(0)  # Go to the beginning of the BytesIO buffer
+        img_base64 = base64.b64encode(img_data.read()).decode('utf-8')
+
+        # Embed the base64-encoded PNG image into the HTML
+        img_html = f"<img src='data:image/png;base64,{img_base64}' alt='{title}'><br>\n"
+        new_html += img_html
+
+    # Close the pie charts section
+    new_html += "</div>\n"
+
+    return new_html
+
 def reformat_df_and_produce_proc_html(study_tracking_df, pipeline_name, ses_label, output_html_path, file_selection_dict):
     
     study_tracking_df.fillna('Not Evaluated', inplace=True)
     study_tracking_df.loc[study_tracking_df["scans_tsv_present"] == 1, "scans_tsv_present"] = 'True'
     study_tracking_df.loc[study_tracking_df["scans_tsv_present"] == 0, "scans_tsv_present"] = 'False'
-    #study_tracking_df.loc[study_tracking_df["derivatives_found"] == 1, "derivatives_found"] = 'True'
-    #study_tracking_df.loc[study_tracking_df["derivatives_found"] == 0, "derivatives_found"] = 'False'
+    study_tracking_df.loc[study_tracking_df["derivatives_found"] == 1, "derivatives_found"] = 'True'
+    study_tracking_df.loc[study_tracking_df["derivatives_found"] == 0, "derivatives_found"] = 'False'
     styler = study_tracking_df.style
     for temp_key in file_selection_dict.keys():
-        styler = color_specific_value_cells(styler, temp_key, 'Satisfied', color='Green')
-        styler = color_specific_value_cells(styler, temp_key, 'Failed QC', color='Red')
-        styler = color_specific_value_cells(styler, temp_key, 'No File', color='Yellow')
+        styler = color_specific_value_cells(styler, study_tracking_df, temp_key, 'Satisfied', color='Green')
+        styler = color_specific_value_cells(styler, study_tracking_df, temp_key, 'Failed QC', color='Red')
+        styler = color_specific_value_cells(styler, study_tracking_df, temp_key, 'No File', color='Yellow')
 
 
     for temp_col in study_tracking_df.columns:
-        styler = color_specific_value_cells(styler, temp_col, 'Not Evaluated', color='Grey')
+        styler = color_specific_value_cells(styler, study_tracking_df, temp_col, 'Not Evaluated', color='Grey')
         if temp_col.startswith('CBRAIN_'):
-            styler = color_specific_value_cells(styler, temp_col, 'Satisfied', color='Green')
-            styler = color_specific_value_cells(styler, temp_col, 'No File', color='Red')
+            styler = color_specific_value_cells(styler, study_tracking_df, temp_col, 'Satisfied', color='Green')
+            styler = color_specific_value_cells(styler, study_tracking_df, temp_col, 'No File', color='Red')
 
 
-    styler = color_specific_value_cells(styler, 'scans_tsv_present', 'True', color='Green')
-    styler = color_specific_value_cells(styler, 'scans_tsv_present', 'False', color='Red')
+    styler = color_specific_value_cells(styler, study_tracking_df, 'scans_tsv_present', 'True', color='Green')
+    styler = color_specific_value_cells(styler, study_tracking_df, 'scans_tsv_present', 'False', color='Red')
 
     red_cbrain_statuses = ['Terminated', 'Failed To Setup', 'Failed To PostProcess', 'Failed Setup Prerequisites', 'Failed PostProcess Prerequisites', 'Suspended', 'Failed', 'Failed On Cluster']
     for temp_status in red_cbrain_statuses:
-        styler = color_specific_value_cells(styler, 'CBRAIN_Status', temp_status, color='Red')
-    styler = color_specific_value_cells(styler, 'CBRAIN_Status', 'Completed', color='Red')
+        styler = color_specific_value_cells(styler, study_tracking_df, 'CBRAIN_Status', temp_status, color='Red')
+        styler = color_specific_value_cells(styler, study_tracking_df, 'CBRAIN_Status', temp_status, color='Red', secondary_column = 'subject')
+    styler = color_specific_value_cells(styler, study_tracking_df, 'CBRAIN_Status', 'Completed', color='Green')
+    styler = color_specific_value_cells(styler, study_tracking_df, 'derivatives_found', 'True', color='Green', secondary_column = 'subject')
+    styler = color_specific_value_cells(styler, study_tracking_df, 'derivatives_found', 'True', color='Green', secondary_column = 'CBRAIN_Status')
+    styler = color_specific_value_cells(styler, study_tracking_df, 'derivatives_found', 'True', color='Green')
 
 
 
@@ -1777,10 +1830,21 @@ def reformat_df_and_produce_proc_html(study_tracking_df, pipeline_name, ses_labe
     text_list.append('"Not Evaluated" entries occur when we identify soemthing in the processing routine that prevents processing from being attempted. This includes (1) previous attempts at processing, (2) missing scans.tsv file, (3) failure to have reqiured BIDS files of sufficient quality in one or more categories of imaging modalities, (4) missing prerequisite outputs from another pipeline.')
     text_list.append('Any columns beginning with "CBRAIN_" represent file collections in CBRAIN that are expected for the current processing pipeline. These columns generally represent the outputs from previous processing pipelines that will be fed to the current pipeline for processing. If a value of "No File" is observed, this either means processing with the other pipeline was not attempted or unsuccessful.')
     text_list.append('There are certain cases where a subject has specific file types that are missing or failing QC but processing still occurred for the subject. This is because there are multiple sets of criteria that are used to determine whether a subject should be processed. For example if a subject has a good T1w and T2w image then both will be included for processing, but processing will still occur if only a good T2w image is available for processing. For a subject to be processed, they must have all the files needed to satisfy at least one of the requirements files for the given pipeline defined at https://github.com/erikglee/HBCD_CBRAIN_PROCESSING/tree/qc_aware_proc/comprehensive_processing_prerequisites . If at least one requirement is satisfied then as many files as possible (up to the limits defined by the "num_to_keep" field) will be included in processing.')
-    #text_list.append()
     html = add_title_and_list_to_html_content(html, title, text_list)
+    
+    #Create dict with instructions for pie charts
+    plot_dict = {}
+    columns_to_skip_plotting = ['subject', 'pipeline', 'session']
+    for temp_col in study_tracking_df.columns:
+        if temp_col not in columns_to_skip_plotting:
+            plot_dict[temp_col] = study_tracking_df[temp_col].value_counts().to_dict()
+    
+    #Add pie charts to html
+    html = append_pie_charts_to_html(html, plot_dict)
+    
+    
     html = add_prettier_background_to_html(html)
-    with open(output_html_path, 'w') as f:
+    with open(log_html_name, 'w') as f:
         f.write(html)
         
     return study_tracking_df
@@ -1957,11 +2021,9 @@ def update_processing(pipeline_name, registered_and_s3_names, registered_and_s3_
                                         derivatives_bucket_config = derivatives_bucket_config):
             subject_processing_details['derivatives_found'] = True
             print('    Already has derivatives')
-            print('    DERIVS EXIST')
             continue
         else:
             subject_processing_details['derivatives_found'] = False
-            print('    DERIVS DONT EXIST')
 
         #Check what type of processing has already occured for the subject with
         #this pipeline and only continue if processing hasn't already been initiated
@@ -1977,6 +2039,7 @@ def update_processing(pipeline_name, registered_and_s3_names, registered_and_s3_
             subj_ses_qc_file_path = download_scans_tsv_file(bids_bucket_config, logs_directory, temp_subject, ses_name, bids_prefix = 'assembly_bids', bucket = bids_bucket, client = None)
             if (type(subj_ses_qc_file_path) == type(None)) and (qc_info_required == True):
                 print('    Skipping Processing - No QC file found for subject')
+                subject_processing_details['scans_tsv_present'] = False
                 continue
             else:
                 subject_processing_details['scans_tsv_present'] = True
