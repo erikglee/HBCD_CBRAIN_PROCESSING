9494cab407695ce8878c19e4f53df95703a3b0ee
Erik Lee <leex6144@umn.edu>
2024-10-03 15:50:51 -0500

    [dev]

--- docs/source/grab_cbrain_tool_details.py
+++ docs/source/grab_cbrain_tool_details.py
@@ -95,7 +95,8 @@ def generate_rst(json_data, tool_config_id, tool_name, url):
             except:
                 f.write(f"     - NA\n")
             f.write(f"     - {temp_output['description']}\n")
-        f.write("\n\n\n\n")
+        f.write(":nbsp::nbsp::nbsp:")
+
 
     
     
