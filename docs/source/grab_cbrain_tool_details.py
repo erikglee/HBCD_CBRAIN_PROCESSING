97d5157e65cba690f2fa98957be7972262ed41f9
Erik Lee <leex6144@umn.edu>
2024-10-07 15:01:02 -0500

    [dev]

--- docs/source/grab_cbrain_tool_details.py
+++ docs/source/grab_cbrain_tool_details.py
@@ -146,14 +146,15 @@ def generate_rst(json_data, tool_config_id, tool_name, url):
                 f.write("")
                 for temp_key in temp_processing_reqs.keys():
                     if 'qc_criteria' in temp_processing_reqs[temp_key].keys():
-                        f.write(f"**{temp_key}     **\n")
+                        f.write(f"{temp_key}     \n")
                         f.write(f"{'~'*(len(temp_key) + 5)}\n\n")
                         if "num_to_keep" in temp_processing_reqs[temp_key].keys():
                             f.write("Processing will look for best {} file(s) to keep using the following criteria.\n".format(temp_processing_reqs[temp_key]["num_to_keep"]))
                         else:
                             f.write("Processing will include all files passing the following criteria.")
                         outer_qc_list = temp_processing_reqs[temp_key]['qc_criteria']
-                        for inner_qc_list in outer_qc_list:
+                        for j, inner_qc_list in enumerate(outer_qc_list):
+                            f.write(f"QC Set {j}\n\n")
                             f.write(".. list-table::\n")
                             f.write("   :header-rows: 1\n\n")
                             f.write("   * - scans.tsv Field\n")
