37d1a4a0d3e1f67bb41bc725af1e964b79895845
Erik Lee <leex6144@umn.edu>
2024-10-07 14:30:41 -0500

    [dev]

--- docs/source/grab_cbrain_tool_details.py
+++ docs/source/grab_cbrain_tool_details.py
@@ -106,13 +106,14 @@ def generate_rst(json_data, tool_config_id, tool_name, url):
                 requirements_files.append(os.path.join(comp_proc_recs_dir, filename))
         requirements_files.sort()
         if len(requirements_files) > 0:
-            f.write("Comprehensive Processing Recommendations\n")
-            f.write("***************************************\n\n")
+            is_qc_used = False
+            f.write("File Selection For Processing\n")
+            f.write("*****************************\n\n")
             f.write("The data for one imaging session (i.e. sub-1, ses-V02) must\n")
             f.write("satisfy at least one of the following requirements for processing.\n")
             f.write("If at least one requirement is satisfied processing can occur.\n")
             f.write("If processing can occur, any files that match any requirements \n")
-            f.write("will be inclued during processing.\n\n")
+            f.write("will be considered for processing.\n\n")
             for filename in requirements_files:
                 with open(filename, 'r') as f2:
                     temp_processing_reqs = json.load(f2)
@@ -126,6 +127,8 @@ def generate_rst(json_data, tool_config_id, tool_name, url):
                 f.write("     - Included (True)/Excluded (False)\n")
                 for temp_key in temp_processing_reqs.keys():
                     file_naming_dict = temp_processing_reqs[temp_key]['file_naming']
+                    if 'qc_criteria' in temp_processing_reqs[temp_key].keys():
+                        is_qc_used = True
                     for i, temp_inner_key in enumerate(file_naming_dict.keys()):
                         if i == 0:
                             f.write(f"   * - {temp_key}\n")
@@ -137,6 +140,31 @@ def generate_rst(json_data, tool_config_id, tool_name, url):
 
 
                 f.write("\n\n")
+            if is_qc_used:
+                f.write("Quality Control Selection Information\n")
+                f.write("**************************************\n\n")
+                f.write("")
+                for temp_key in temp_processing_reqs.keys():
+                    if 'qc_criteria' in temp_processing_reqs[temp_key].keys():
+                        f.write(f"**{temp_key}**\n")
+                        f.write(f"{'~'*len(temp_key)}\n\n")
+                        if "num_to_keep" in temp_processing_reqs[temp_key].keys():
+                            f.write("Processing will look for best {} file(s) to keep using the following criteria.".format(temp_processing_reqs[temp_key]["num_to_keep"]))
+                        else:
+                            f.write("Processing will include all files passing the following criteria.")
+                        outer_qc_list = temp_processing_reqs[temp_key]['qc_criteria']
+                        for inner_qc_list in outer_qc_list:
+                            f.write(".. list-table::\n")
+                            f.write("   :header-rows: 1\n\n")
+                            f.write("   * - scans.tsv Field\n")
+                            f.write("     - Value\n")
+                            f.write("     - Operator\n")
+                            for temp_qc in inner_qc_list:
+                                key = next(iter(temp_qc))
+                                f.write(f"   * - {key}\n")
+                                f.write(f"     - {temp_qc[key][0]}\n")
+                                f.write(f"     - {temp_qc[key][1]}\n")
+                            f.write("\n\n")
 
 
     
